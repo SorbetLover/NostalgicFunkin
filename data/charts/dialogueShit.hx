6177e1cb4f96e76b46ecaf70e8d0db80ada14c22
unknown <mmrw949@gmail.com>
2023-08-21 17:59:19 +0300

    starving dialogue implementgit pushgit push

---
+++ data/charts/dialogueShit.hx
@@ -0,0 +1,19 @@
+function postCreate() {
+    if (playCutscenes) {
+        iconP1.alpha = 0;
+        iconP2.alpha = 0;
+        for (d in [cpuStrums, playerStrums])
+            d.visible = false;
+    } else {
+        finishDialogue();
+        // trace("dsad");
+    }
+}
+
+function finishDialogue() {
+    iconP1.alpha = 1;
+    iconP2.alpha = 1;
+    playerStrums.forEach(function(strum:StrumNote) {
+        FlxTween(strum, {alpha: 1}, 0.5);
+    });
+}
