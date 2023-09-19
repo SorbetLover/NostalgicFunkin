25b9e693b5aa235a582a31e0d5492eccee3e1cf1
unknown <mmrw949@gmail.com>
2023-09-19 17:15:08 +0300

    slightly better freeplay bg color transition

--- data/states/FreeplayState.hx
+++ data/states/FreeplayState.hx
@@ -2,14 +2,32 @@ import flixel.util.FlxColor;
 import flixel.util.FlxAxes;
 import flixel.text.FlxTextBorderStyle;
 
-var boxPage:FlxSprite;
+// var boxPage:FlxSprite;
 
-var weekBg:String = "default";
+var modBg_img_name:String = "default";
 var currentWeek:String;
-var freeplayText:FlxText;
-// var indexs:Array = [5, 9, 15];
 
-function create() {
+var modBg:FlxSprite = new FlxSprite();
+var modTitle:FlxText;
+
+function postCreate() {
+    modBg.antialiasing = true;
+    modBg.blend ="overlay";
+    insert(members.indexOf(bg) + 1, modBg);
+
+
+    CoolUtil.loadAnimatedGraphic(bg, Paths.image("menus/squrefreeplaybg"));
+    modBg.color = FlxColor.fromString("#050505");
+    for (a in [bg, modBg]) {
+        a.alpha = 0.7;
+    }
+
+    modTitle = new FlxText(440, 5, 700);
+    modTitle.setFormat("fonts/freeplay.ttf", 25, FlxColor.WHITE, "CENTER");
+    modTitle.setBorderStyle(FlxTextBorderStyle.OUTLINE, FlxColor.BLACK, 2);
+    modTitle.antialiasing = true;
+    add(modTitle);
+
     // boxPage = new FlxSprite(scoreText.x - 7, 100).makeGraphic(600, 600, 0xFF000000);
     // boxPage.updateHitbox();
     // boxPage.alpha = 0.6;
@@ -21,54 +39,40 @@ function create() {
     //     text.antialiasing = true;
     //     add(text);
     // }
-
-}
-
-function postCreate() {
-    freeplayText = new FlxText(490, 5, 700);
-    freeplayText.setFormat("fonts/freeplay.ttf", 25, FlxColor.WHITE, "CENTER");
-    freeplayText.setBorderStyle(FlxTextBorderStyle.OUTLINE, FlxColor.BLACK, 2);
-    freeplayText.antialiasing = true;
-    add(freeplayText);
+    // FlxTween.tween(modBg, {alpha: 0.7}, 1);
 }
 
 function postUpdate() {
+    modBg.loadGraphic(Paths.image("menus/freeplay/" + modBg_img_name));
     // trace(songs[curSelected].name + " || " + currentWeek);
 
-    CoolUtil.loadAnimatedGraphic(bg, Paths.image("menus/freeplay/" + weekBg));
-    freeplayText.text = "Current week: " + currentWeek;
+    modTitle.text = "Current week: " + currentWeek;
 
     switch(songs[curSelected].name) {
         case "tutorial-b" | "bopeebo-b" | "fresh-b" | "dadbattle-b" | "spookeez-b" | "south-b" | "pico-b" | "philly-b" | "blammed-b" | "satin-panties-b" | "high-b" | "milf-b":
             currentWeek = "B Sides";
-            weekBg = "default";
+            modBg_img_name = "default";
 
         case "lo-fight" | "overhead" | "ballistic" | "remorse":
             currentWeek = "Vs Whitty";
-            weekBg = "whitty";
+            modBg_img_name = "whitty";
 
         case "carol-roll" | "body" | "boogie":
             currentWeek = "Vs Carol";
-            weekBg = "default";
+            modBg_img_name = "default";
 
         case "my-battle" | "last-chance" | "genocide":
             currentWeek = "Vs Tabi";
-            weekBg = "tabi";
+            modBg_img_name = "tabi";
 
         case "foolhardy" | "Bushwhack":
             currentWeek = "Vs Foolhardy";
-            weekBg = "foolhardy";
+            modBg_img_name = "foolhardy";
 
         default:
             currentWeek = "some other week";
-            weekBg = "default";
+            modBg_img_name = "default";
     }
-    // if (FlxG.keys.justPressed.E && curSelected != null) {
-    //     changeSelection(3);
-    // }
 
-    // else if (FlxG.keys.justPressed.Q && curSelected != null) {
-    //     changeSelection(-3);
-    // }
     changeSelection( (FlxG.keys.justPressed.Q ? -3 : 0) + (FlxG.keys.justPressed.E ? 3 : 0) );
 }
