85c9489b2a4a301596a22e097042072a4dda28fa
MAZ12211 <mmrw949@gmail.com>
2023-11-16 14:37:01 +0200

    port back in Whitty's Kade chart + rechart ballisitc

--- songs/kadeUI.hx
+++ songs/kadeUI.hx
@@ -1,4 +1,4 @@
-// Kade UI + some behavioral changes from Kade (not finished at all, but I'll get back into it when it's pirority)
+// Kade UI + some behavioral changes from Kade (not finished at all, but I'll get back to this from time to time/when it's pirority)
 
 public var kadeUI:Bool = FlxG.save.data.kadeUI;
 
@@ -6,26 +6,20 @@ function postUpdate(elapsed) {
     if (!kadeUI) disableScript();
 
     // I don't remember this exactly but it's old-old icon bop that I happened to make when porting another mod
-    if (kadeUI) {
-        iconP1.scale.set(lerp(iconP1.scale.x, 1, 0.33), lerp(iconP1.scale.y, 1, 0.33));
-        iconP2.scale.set(lerp(iconP2.scale.x, 1, 0.33), lerp(iconP2.scale.y, 1, 0.33));
-    }
+    iconP1.scale.set(lerp(iconP1.scale.x, 1, 0.33), lerp(iconP1.scale.y, 1, 0.33));
+    iconP2.scale.set(lerp(iconP2.scale.x, 1, 0.33), lerp(iconP2.scale.y, 1, 0.33));
 }
 
-function onDadHit(e) {
-    e.preventStrumGlow();
-}
+function onDadHit(e) e.preventStrumGlow();
 
 function onPostStrumCreation(e) {
 
     // More of a personal thing but bascially bigger frames for pressing notes to mimick botplay's sudden note presses
     // Will be changed later probably
     if (kadeUI && FlxG.save.data.fastPress) e.strum.animation.addByIndices("confirm", e.animPrefix + " confirm", [0, 1, 2, 3], "", 60, false);
-    if (kadeUI) e.strum.animation.addByIndices("pressed", e.animPrefix + " press", [0, 1, 2, 3], "", 12, false); // 12 frames when pressing (pretty minor detail :shrug:)
+    e.strum.animation.addByIndices("pressed", e.animPrefix + " press", [0, 1, 2, 3], "", 12, false); // 12 frames when pressing (pretty minor detail :shrug:)
 
     // Characters will immidately go to their idle animation after pressing/holding notes.
-    if (kadeUI) {
-        boyfriend.holdTime = 0.5;
-        dad.holdTime = 0.8;
-    }
+    boyfriend.holdTime = 0.5;
+    dad.holdTime = 0.8;
 }
