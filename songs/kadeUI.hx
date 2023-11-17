d018f8796950f4f68b0faddef3fee53766069963
MAZ12211 <mmrw949@gmail.com>
2023-11-17 09:23:11 +0200

    bleh

--- songs/kadeUI.hx
+++ songs/kadeUI.hx
@@ -5,30 +5,37 @@ public var kadeUI:Bool = FlxG.save.data.kadeUI;
 function postUpdate(elapsed) {
     if (!kadeUI) disableScript();
 
-    // I don't remember this exactly but it's old-old icon bop that I happened to make when porting another mod
-    iconP1.scale.set(lerp(iconP1.scale.x, 1, 0.33), lerp(iconP1.scale.y, 1, 0.33));
-    iconP2.scale.set(lerp(iconP2.scale.x, 1, 0.33), lerp(iconP2.scale.y, 1, 0.33));
+
+    if (kadeUI) {
+        // I don't remember this exactly but it's old-old icon bop that I happened to make when porting another mod
+        iconP1.scale.set(lerp(iconP1.scale.x, 1, 0.33), lerp(iconP1.scale.y, 1, 0.33));
+        iconP2.scale.set(lerp(iconP2.scale.x, 1, 0.33), lerp(iconP2.scale.y, 1, 0.33));
+    }
 }
 
-function onDadHit(e) e.preventStrumGlow();
+function onDadHit(e) {
+    if (kadeUI) e.preventStrumGlow();
+}
 
 function onPostNoteCreation(e) {
-    // e.note.animation.getByName("hold").y -= 80;
+    if (kadeUI) {
+            // e.note.animation.getByName("hold").y -= 80;
     if (e.note.isSustainNote || e.note.nextNote?.isSustainNote) {
-        // e.note.scale.y = 1;
-        e.note.height -= 22.4;
+        e.note.scale.y -= 1;
+        // e.note.height -= 22.4;
+    }
     }
 }
 
 function onPostStrumCreation(e) {
-if (kadeUI) {
+    if (kadeUI) {
         // More of a personal thing but bascially bigger frames for pressing notes to mimick botplay's sudden note presses
-    // Will be changed later probably
-    if (kadeUI && FlxG.save.data.fastPress) e.strum.animation.addByIndices("confirm", e.animPrefix + " confirm", [0, 1, 2, 3], "", 60, false);
-    e.strum.animation.addByIndices("pressed", e.animPrefix + " press", [0, 1, 2, 3], "", 12, false); // 12 frames when pressing (pretty minor detail :shrug:)
+        // Will be changed later probably
+        if (kadeUI && FlxG.save.data.fastPress) e.strum.animation.addByIndices("confirm", e.animPrefix + " confirm", [0, 1, 2, 3], "", 60, false);
+        e.strum.animation.addByIndices("pressed", e.animPrefix + " press", [0, 1, 2, 3], "", 12, false); // 12 frames when pressing (pretty minor detail :shrug:)
 
-    // Characters will immidately go to their idle animation after pressing/holding notes.
-    boyfriend.holdTime = 1;
-    dad.holdTime = 1;
-}
+        // Characters will immidately go to their idle animation after pressing/holding notes.
+        boyfriend.holdTime = 1;
+        dad.holdTime = 1;
+    }
 }
