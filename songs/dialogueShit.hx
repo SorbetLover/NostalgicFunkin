fed4e807158b4bfa9e8e1d4a77ae781de5486fc6
MAZ12211 <mmrw949@gmail.com>
2023-11-18 18:18:21 +0200

    ballistic cutscene done!!

--- songs/dialogueShit.hx
+++ songs/dialogueShit.hx
@@ -1,6 +1,6 @@
 import funkin.game.cutscenes.DialogueCutscene;
 var cameraExists = false;
-public var colorBgAlpha:Float = 0.8;
+public var dialogueBgAlpha:Float = 0.5;
 
 function update(elapsed) {
     if (subState != null) cameraExists = true;
@@ -8,7 +8,7 @@ function update(elapsed) {
 
     switch(cameraExists) {
         case true: /* if dialogue camera exists */
-            if (subState.dialogueCamera != null /*|| !(buncha songs that use background images for the dialogue)*/ )  subState.dialogueCamera.bgColor = FlxColor.fromRGBFloat(255, 255, 255, colorBgAlpha);
+            if (subState.dialogueCamera != null /*|| !(buncha songs that use background images for the dialogue)*/ )  subState.dialogueCamera.bgColor = FlxColor.fromRGBFloat(255, 255, 255, dialogueBgAlpha);
 
         case false: /* if dialogue camera doesn't exist */
 
