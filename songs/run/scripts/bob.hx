f4a6d43d14f8b090880973bfbee155916a30c6ff
MAZ12211 <mmrw949@gmail.com>
2023-12-05 16:00:28 +0200

    Bob shake screen, screen FX fix, global scirpt works now I think, recharter creditor engage!!

--- songs/run/scripts/bob.hx
+++ songs/run/scripts/bob.hx
@@ -1,9 +1,9 @@
+import openfl.Lib;
 var isbobmad:Bool = true;
 var bobmadshake:FlxSprite;
 var bobsound:FlxSound;
 
 function create() {
-    trace(FlxG.save.data.shakingscreen);
     bobmadshake = new FlxSprite( -198, -118).loadGraphic(Paths.image('stages/bob/hell/hello'));
     bobmadshake.scrollFactor.set(0, 0);
     bobmadshake.visible = false;
@@ -13,7 +13,7 @@ function create() {
 }
 
 function beatHit(b) {
-    if (b % gfSpeed == 0 && !FlxG.save.data.shakingscreen)
+    if (b % gfSpeed == 0 && FlxG.save.data.shakeShit)
 	{
 		camHUD.shake(0.02, 0.2);
 		FlxG.camera.shake(0.005, 0.2);
@@ -40,7 +40,7 @@ function Bobismad()
 	bobsound.play();
 	bobsound.volume = 1;
 	isbobmad = false;
-	//shakescreen();
+	if (!FlxG.fullscreen) shakescreen();
 	new FlxTimer().start(0.5 , function(tmr:FlxTimer)
 	{
 		resetBobismad();
@@ -51,6 +51,6 @@ function shakescreen()
 {
 	new FlxTimer().start(0.01, function(tmr:FlxTimer)
 	{
-		//Lib.application.window.move(Lib.application.window.x + FlxG.random.int( -10, 10),Lib.application.window.y + FlxG.random.int( -8, 8));
+		Lib.application.window.move(Lib.application.window.x + FlxG.random.int( -10, 10),Lib.application.window.y + FlxG.random.int( -8, 8));
 	}, 50);
 }
