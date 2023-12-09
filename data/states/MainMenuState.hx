aa62180e89c6396f9b1d6418aa63260e96807575
MAZ12211 <mmrw949@gmail.com>
2023-12-09 11:43:44 +0200

    init freeplaySelectState

---
+++ data/states/MainMenuState.hx
@@ -0,0 +1,7 @@
+function onSelectItem(e) {
+    if (e.name == "freeplay") {
+        // FlxG.switchState(new ModState("FreeplaySelectState"));
+        // e.cancel();
+        // trace("switching to FreeplaySelectState");
+    }
+}
