bfba612fbdc61976ec85e74a3fd3fadba0a7ea46
MAZ12211 <mmrw949@gmail.com>
2023-12-27 21:43:03 +0200

    decrease load time for low memory mod on Kapi

--- data/stages/Hairball.hx
+++ data/stages/Hairball.hx
@@ -3,36 +3,33 @@ var littleGuys:FlxSprite;
 var lights:FlxSprite;
 
 function postCreate() {
-    lights = new FlxSprite(-600, -200);
-    lights.frames = Paths.getSparrowAtlas('stages/week1/lights');
-    lights.animation.addByPrefix('light', "lightblink1", 1, false);
-    lights.antialiasing = true;
-    lights.scrollFactor.set(0.9, 0.9);
-    lights.setGraphicSize(Std.int(lights.width * 1));
-    lights.updateHitbox();
-    insert(members.indexOf(gf) - 1, lights);
-
-    upperBoppers = new FlxSprite(-600, -200);
-    upperBoppers.frames = Paths.getSparrowAtlas('stages/week1/upperBop');
-    upperBoppers.animation.addByPrefix('bop', "Upper Crowd Bob", 24, false);
-    upperBoppers.antialiasing = true;
-    upperBoppers.scrollFactor.set(1.05, 1.05);
-    upperBoppers.setGraphicSize(Std.int(upperBoppers.width * 1));
-    upperBoppers.updateHitbox();
-    add(upperBoppers);
-     
-    littleGuys = new FlxSprite(25, 200);
-    littleGuys.frames = Paths.getSparrowAtlas('stages/week1/littleguys');
-    littleGuys.animation.addByPrefix('bop', 'Bottom Level Boppers', 24, false);
-    littleGuys.antialiasing = true;
-    littleGuys.scrollFactor.set(0.9, 0.9);
-    littleGuys.setGraphicSize(Std.int(littleGuys.width * 1));
-    littleGuys.updateHitbox();
-    insert(members.indexOf(lights) + 1, littleGuys);
-    if (Options.lowMemoryMode) {
-        remove(upperBoppers);
-        remove(littleGuys);
-        remove(lights);
+    if (!Options.lowMemoryMode) {
+        lights = new FlxSprite(-600, -200);
+        lights.frames = Paths.getSparrowAtlas('stages/week1/lights');
+        lights.animation.addByPrefix('light', "lightblink1", 1, false);
+        lights.antialiasing = true;
+        lights.scrollFactor.set(0.9, 0.9);
+        lights.setGraphicSize(Std.int(lights.width * 1));
+        lights.updateHitbox();
+        insert(members.indexOf(gf) - 1, lights);
+    
+        upperBoppers = new FlxSprite(-600, -200);
+        upperBoppers.frames = Paths.getSparrowAtlas('stages/week1/upperBop');
+        upperBoppers.animation.addByPrefix('bop', "Upper Crowd Bob", 24, false);
+        upperBoppers.antialiasing = true;
+        upperBoppers.scrollFactor.set(1.05, 1.05);
+        upperBoppers.setGraphicSize(Std.int(upperBoppers.width * 1));
+        upperBoppers.updateHitbox();
+        add(upperBoppers);
+         
+        littleGuys = new FlxSprite(25, 200);
+        littleGuys.frames = Paths.getSparrowAtlas('stages/week1/littleguys');
+        littleGuys.animation.addByPrefix('bop', 'Bottom Level Boppers', 24, false);
+        littleGuys.antialiasing = true;
+        littleGuys.scrollFactor.set(0.9, 0.9);
+        littleGuys.setGraphicSize(Std.int(littleGuys.width * 1));
+        littleGuys.updateHitbox();
+        insert(members.indexOf(lights) + 1, littleGuys);
     }
 }
 
--- songs/nyaw/scripts/modchart.hx
+++ songs/nyaw/scripts/modchart.hx
@@ -1,15 +1,16 @@
 var lights:FlxSprite;
 
 function postCreate() {
-    lights = new FlxSprite(-600, -200);
-    lights.frames = Paths.getSparrowAtlas('stages/week1/lights');
-    lights.animation.addByPrefix('light', "lightblink1", 1, false);
-    lights.antialiasing = true;
-    lights.scrollFactor.set(0.9, 0.9);
-    lights.setGraphicSize(Std.int(lights.width * 1));
-    lights.updateHitbox();
-    insert(members.indexOf(gf) - 1, lights);
-    if (Options.lowMemoryMode) remove(lights);
+    if (!Options.lowMemoryMode) {
+        lights = new FlxSprite(-600, -200);
+        lights.frames = Paths.getSparrowAtlas('stages/week1/lights');
+        lights.animation.addByPrefix('light', "lightblink1", 1, false);
+        lights.antialiasing = true;
+        lights.scrollFactor.set(0.9, 0.9);
+        lights.setGraphicSize(Std.int(lights.width * 1));
+        lights.updateHitbox();
+        insert(members.indexOf(gf) - 1, lights);
+    }
 }
 
 function beatHit(curBeat) {
