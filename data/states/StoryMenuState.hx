1c589dea00931b667f90cc50796b131084b1faf3
MAZ12211 <mmrw949@gmail.com>
2024-02-16 20:17:36 +0200

    foolhardy & bushwhack story menu fix finally

---
+++ data/states/StoryMenuState.hx
@@ -0,0 +1,7 @@
+function update() {
+    if (weekTitle.text == "Hardest Mod In The Corn Field" || weekTitle.text == "Sequel To The Hardest Mod In The Corn Field") {
+        weekBG.color = FlxColor.black;
+    } else {
+        weekBG.color =  0xFFF9CF51;
+    }
+}
