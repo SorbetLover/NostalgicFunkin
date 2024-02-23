48e666bb3607ca5c6bba3c579626a92269ea3046
MAZ12211 <mmrw949@gmail.com>
2024-02-23 18:45:50 +0200

    Revert "updated rechart text for meta data support"
    This reverts commit ecdd212d54a5cfbc9ea25ebceb4587b89646fa8c.

--- data/states/MetaDataScreen.hx
+++ data/states/MetaDataScreen.hx
@@ -3,7 +3,6 @@ import funkin.editors.ui.UIText;
 
 var didAlr = false;
 var weekTextBox;
-var recharterTextBox;
 
 function postUpdate(){
     if(!didAlr)
@@ -13,19 +12,13 @@ function postUpdate(){
 function postCreate() {
     didAlr = true;
 
-    weekTextBox = new UITextBox(needsVoicesCheckbox.x-150,needsVoicesCheckbox.y+50, metadata.week, 130);
+    weekTextBox = new UITextBox(needsVoicesCheckbox.x,needsVoicesCheckbox.y+80, metadata.week, 130);
     add(weekTextBox);
     add(new UIText(weekTextBox.x, weekTextBox.y - 24, 0, "week"));
-
-    recharterTextBox = new UITextBox(weekTextBox.x+140, weekTextBox.y, metadata.recharter, 130);
-    add(recharterTextBox);
-    add(new UIText(recharterTextBox.x, recharterTextBox.y - 24, 0, "recharter"));
 
     var ogFunc = saveButton.callback;
     saveButton.callback = function(){
         ogFunc();
 		PlayState.SONG.meta.week = weekTextBox.label.text;
-        if(!(recharterTextBox.label.text == null || recharterTextBox.label.text == ''))
-		    PlayState.SONG.meta.recharter = recharterTextBox.label.text;
     };
 }
--- songs/recharter_creditor.hx
+++ songs/recharter_creditor.hx
@@ -6,34 +6,36 @@
     It's meant to show more appreaction to the recharter of the songs
     and (hopefully) more achknowledgement from players.
 */
-
 import flixel.text.FlxTextBorderStyle;
 import flixel.text.FlxText.FlxTextAlign;
 
-var recharter:FunkinText = PlayState.SONG.meta.recharter; // "recharter": "name"
+var recharter:FunkinText = Json.parse(Assets.getText(Paths.chart(curSong, PlayState.difficulty))).recharter; // "recharter": "name"
 var noteIco:Note;
 
 function postCreate() {
-    trace(PlayState.SONG.meta.recharter);
-    if(recharter == null) recharter = Json.parse(Assets.getText(Paths.chart(curSong, PlayState.difficulty))).recharter; // "recharter": "name"
-
     noteIco = new Note(playerStrums, {id: FlxG.random.int(0, 3)});
     noteIco.cameras = [camHUD];
     noteIco.setPosition(Std.int(healthBarBG.width / 2), healthBarBG.y - 80);
     noteIco.setGraphicSize(Std.int(noteIco.width * 0.32));
-    noteIco.alpha = 0.000000001;
-    insert(members.indexOf(iconP2) + 1, noteIco);
+    insert(members.indexOf(iconP1) - 1, noteIco);
 
-    recharter = new FunkinText(noteIco.x + noteIco.width - 35,noteIco.y + noteIco.height - 65,healthBarBG.width,recharter,null, false);
+    recharter = new FunkinText(
+        noteIco.x + noteIco.width - 35,
+        noteIco.y + noteIco.height - 65,
+        healthBarBG.width,
+        recharter,
+        null, false
+    );
+
     recharter.setFormat(Paths.font("freeplay.ttf"), 19, FlxColor.WHITE, "LEFT");
     recharter.setBorderStyle(FlxTextBorderStyle.OUTLINE, FlxColor.BLACK, 1);
     recharter.cameras = [camHUD];
-    recharter.alpha = 0.00000001;
     insert(members.lastIndexOf(noteIco), recharter);
 
-    for (a in [noteIco, recharter]) FlxTween.tween(a, {alpha: 1}, (Conductor.crochet / 1000)*introLength,{ease: FlxEase.cubeInOut});
+    noteIco.alpha = 0;
+    recharter.alpha = 0;
 
-    if (recharter.text == "" || recharter.text == null || PlayState.difficulty.toLowerCase() != 'rechart') {
+    if (recharter.text == "" || recharter.text == null) {
         disableScript();
         noteIco.kill();
     }
@@ -41,11 +43,23 @@ function postCreate() {
 
 function beatHit(curBeat) {
     if (curBeat % 1 == 0) {
-        for (a in [noteIco, recharter]){
-            var note:Int = noteIco.noteData;
-            var xDir:Int = note == 0 ? -5 : (note == 3 ? 5 : 0);
-            var yDir:Int = note == 2 ? -5 : (note == 1 ? 5 : 0);
-            FlxTween.tween(a, {x:a.x + xDir,y:a.y + yDir}, Conductor.crochet / 1000, {type: FlxTween.BACKWARD, ease: FlxEase.cubeInOut});
+        switch (noteIco.noteData) {
+            case 0:
+                for (a in [noteIco, recharter])
+                    FlxTween.tween(a, {x: a.x - 5}, Conductor.crochet / 1000, {type: FlxTween.BACKWARD, ease: FlxEase.cubeInOut});
+            case 1:
+                for (a in [noteIco, recharter])
+                    FlxTween.tween(a, {y: a.y + 5}, Conductor.crochet / 1000, {type: FlxTween.BACKWARD, ease: FlxEase.cubeInOut});
+            case 2:
+                for (a in [noteIco, recharter])
+                    FlxTween.tween(a, {y: a.y - 5}, Conductor.crochet / 1000, {type: FlxTween.BACKWARD, ease: FlxEase.cubeInOut});
+            case 3:
+                for (a in [noteIco, recharter])
+                    FlxTween.tween(a, {x: a.x + 5}, Conductor.crochet / 1000, {type: FlxTween.BACKWARD, ease: FlxEase.cubeInOut});
         }
     }
+}
+
+function onStartSong() {
+    for (a in [noteIco, recharter]) FlxTween.tween(a, {alpha: 1}, Conductor.crochet / 4000);
 }
