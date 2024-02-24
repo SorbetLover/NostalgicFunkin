283a3afdcff776c1fd9e71b262aecb99cd8700b8
7oltan <87986834+7oltan@users.noreply.github.com>
2024-02-24 08:52:55 +0300

    blue wip

--- data/events/changeAnimationIndex.hx
+++ data/events/changeAnimationIndex.hx
@@ -1,8 +1,15 @@
-import funkin.backend.scripting.events.PlayAnimEvent;
 import funkin.backend.system.Logs;
 
 function onEvent(eventEvent) {
     if (eventEvent.event.name == "changeAnimationIndex") {
         Logs.trace("This event is deprecated. Use CNE's built-in 'Alt Animation Toggle' instead.", 1);
+		/*var strLine = strumLines.members[eventEvent.event.params[0]];
+		if (strLine != null) {
+		    if (strLine.characters != null) // Alt anim Idle
+				for (character in strLine.characters) {
+					if (character == null) continue;
+					character.idleSuffix = eventEvent.event.params[1];
+				}
+		}*/
     }
 }
