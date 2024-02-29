3cedb19739cfbe77df022c5920691596c9827082
MAZ12211 <mmrw949@gmail.com>
2024-02-29 15:37:38 +0200

    fix monika stages + fix b sides week 6 stages + remove assets from toni again

--- data/stages/school.hx
+++ data/stages/school.hx
@@ -1,17 +1,10 @@
 function create() {
 	importScript("data/scripts/pixel");
-	if (PlayState.SONG.meta.name.toLowerCase() == "bara-no-yume") {
+	if (PlayState.SONG.meta.name.toLowerCase() == "roses-b") {
 		bgGirls.animation.remove("danceLeft");
 		bgGirls.animation.remove("danceRight");
 		bgGirls.animation.addByIndices('danceLeft', 'BG fangirls dissuaded', CoolUtil.numberArray(14), "", 24, false);
 		bgGirls.animation.addByIndices('danceRight', 'BG fangirls dissuaded', CoolUtil.numberArray(30, 15), "", 24, false);
 	}
 	bgGirls.animation.play("danceLeft", true); // horrible fix, please fix later
-}
-
-function postCreate(){
-	if(PlayState.SONG.meta.name == "your-reality"){
-		boyfriend.x += 100;
-		boyfriend.y -= 50;
-	}
 }
