d6cb321172bae84e8aa13b85f93ee19af741d5d7
7oltan <87986834+7oltan@users.noreply.github.com>
2024-03-01 11:30:35 +0300

    small storymenu fix

--- data/states/StoryMenuState.hx
+++ data/states/StoryMenuState.hx
@@ -1,3 +1,14 @@
+function draw(){
+    var text:String = 'TRACKS\n';
+    for(e in weeks[curWeek].songs) {
+        if (!e.hide) {
+            var split = e.name.toUpperCase().split('/');
+            text = text + '\n' + split[split.length-1];
+        }
+    }
+    tracklist.text = text;
+}
+
 function update() {
     if (weekTitle.text == "Hardest Mod In The Corn Field" || weekTitle.text == "Sequel To The Hardest Mod In The Corn Field") {
         weekBG.color = FlxColor.black;
