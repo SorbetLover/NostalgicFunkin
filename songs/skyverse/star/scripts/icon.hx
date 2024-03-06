da137f6d3a8977b0123d20cfe627f4ea65f63293
MAZ12211 <mmrw949@gmail.com>
2024-03-06 07:28:06 +0200

    remove SkyVerse completely

--- songs/skyverse/star/scripts/icon.hx
+++
@@ -1,8 +0,0 @@
-function postCreate(){
-    iconP1.visible = false;
-    iconP2.y -= 20;
-    healthBar.createFilledBar(dad.iconColor,dad.iconColor);
-}
-
-function postUpdate()
-    iconP2.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 1, 0))) - (iconP2.width/2);
