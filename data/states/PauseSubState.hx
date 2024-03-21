b13b465da73284ace5d9b96b0bf786bbac67af30
MAZ12211 <mmrw949@gmail.com>
2024-03-21 21:19:47 +0200

    have starving artist its own folder + minor pause stuff

--- data/states/PauseSubState.hx
+++ data/states/PauseSubState.hx
@@ -1,11 +1,12 @@
 /* 
-    Skips the current song for the purpose of testing cutscenes 
-    for the next song quickly (useless for freeplay)
+    Ends the current song for the purpose of testing
+    end-cutscene.hx for current song and cutscene.hx for the next song
+    without playing the entire song (useless for freeplay)
 */
 var isDebugEnabled:Bool = true; // set to false before release
 
 function create()
-    if (game.isStoryMode && isDebugEnabled && game.inst != null && game.vocals != null) menuItems.insert(1, 'Skip Song');
+    if (game.isStoryMode && isDebugEnabled && game.inst != null && game.vocals != null) menuItems.insert(1, 'End Song');
 
 function postUpdate()
     if (controls.ACCEPT)
